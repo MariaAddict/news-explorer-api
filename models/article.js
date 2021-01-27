cdd872515854b91f5d7f126f8cf5fcd80403b2fa
MariaAddict <mashakan27@gmail.com>
2021-01-27 16:22:28 +0700

    feat: создать схему и модель статьи

---
+++ models/article.js
@@ -0,0 +1,60 @@
+const mongoose = require('mongoose');
+
+const articleSchema = new mongoose.Schema({
+  keyword: {
+    type: String,
+    required: true,
+    minlength: 1,
+    maxlength: 30,
+  },
+  title: {
+    type: String,
+    required: true,
+    minlength: 1,
+    maxlength: 30,
+  },
+  text: {
+    type: String,
+    required: true,
+    minlength: 1,
+  },
+  data: {
+    type: Date,
+    required: true,
+    default: Date.now,
+  },
+  source: {
+    type: String,
+    required: true,
+    minlength: 1,
+    maxlength: 30,
+  },
+  link: {
+    type: String,
+    validate: {
+      validator(v) {
+        return /https?:\/\/(w{3})?\.?[0-9A-Za-z\-._~:/?#[\]@!$&'()*,+;=]#?/g.test(v);
+      },
+      message: 'Неправильный url',
+    },
+    required: true,
+  },
+  image: {
+    type: String,
+    validate: {
+      validator(v) {
+        return /https?:\/\/(w{3})?\.?[0-9A-Za-z\-._~:/?#[\]@!$&'()*,+;=]#?/g.test(v);
+      },
+      message: 'Неправильный url',
+    },
+    required: true,
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user',
+    required: true,
+    select: false,
+  },
+});
+
+module.exports = mongoose.model('article', articleSchema);
