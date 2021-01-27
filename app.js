46c28e92e8134bfb9a9bad152ad18b93669cf33c
MariaAddict <mashakan27@gmail.com>
2021-01-27 18:17:54 +0700

    feat: добавить централизованную обработку ошибок

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const { PORT = 3000 } = process.env;
 const mongoose = require('mongoose');
 
 const usersRouter = require('./routes/users.js');
+const NotFoundError = require('./errors/not-found');
 
 const app = express();
 mongoose.connect('mongodb://localhost:27017/news-explorerdb', {
@@ -26,6 +27,19 @@ app.use((req, res, next) => {
 });
 
 app.use('/', usersRouter);
+
+app.use(() => {
+  throw new NotFoundError('Запрашиваемый ресурс не найден');
+});
+
+app.use((err, req, res, next) => {
+  if (err.statusCode) {
+    res.status(err.statusCode).send({ message: err.message });
+    return;
+  }
+  res.status(500).send({ message: `На сервере произошла ошибка: ${err.message}` });
+  next();
+});
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
