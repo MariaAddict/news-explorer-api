68d62776e83111db7bd26828b88152aa3e81b394
MariaAddict <mashakan27@gmail.com>
2021-01-29 23:00:30 +0700

    feat: установить cors

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 const bodyParser = require('body-parser');
 const { errors } = require('celebrate');
+const cors = require('cors');
 
 const { PORT = 3000 } = process.env;
 const mongoose = require('mongoose');
@@ -16,6 +17,8 @@ mongoose.connect('mongodb://localhost:27017/news-explorerdb', {
   useFindAndModify: false,
   useUnifiedTopology: true,
 });
+
+app.use(cors({ origin: true }));
 
 app.use(bodyParser.json());
 app.use(requestLogger);
