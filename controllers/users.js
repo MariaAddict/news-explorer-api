0c752431f0b31fa30794e38a199692e564993506
MariaAddict <mashakan27@gmail.com>
2021-01-29 19:31:55 +0700

    feat: создать контроллер userLogin и роутер signin

--- controllers/users.js
+++ controllers/users.js
@@ -1,4 +1,6 @@
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const JWT_SECRET = require('crypto').randomBytes(32).toString('hex');
 const User = require('../models/user');
 const NotFoundError = require('../errors/not-found');
 const ValidationError = require('../errors/validation-error');
@@ -55,4 +57,35 @@ const createUser = (req, res, next) => {
     .catch(next);
 };
 
-module.exports = { getUser, createUser };
+const userLogin = (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    const err = new Error('Некорректные данные пользователя');
+    err.statusCode = 400;
+    return next(err);
+  }
+
+  return User.findOne({ email }).select('+password')
+    .then((user) => {
+      if (!user) {
+        const err = new Error('Некорректные данные пользователя');
+        err.statusCode = 401;
+        return next(err);
+      }
+
+      return bcrypt.compare(password, user.password)
+        .then((matched) => {
+          if (!matched) {
+            const err = new Error('Некорректные данные пользователя');
+            err.statusCode = 401;
+            return next(err);
+          }
+          const token = jwt.sign({ _id: user._id }, JWT_SECRET, { expiresIn: '7d' });
+          return res.send({ token });
+        });
+    })
+    .catch(next);
+};
+
+module.exports = { getUser, createUser, userLogin };
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
 const usersRouter = require('./users');
 const articlesRouter = require('./articles');
-const { createUser } = require('../controllers/users');
+const { createUser, userLogin } = require('../controllers/users');
 
+router.post('/signin', userLogin);
 router.post('/signup', createUser);
 router.use('/', usersRouter);
 router.use('/', articlesRouter);
