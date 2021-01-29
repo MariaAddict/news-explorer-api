102934d0f96a70d078438fcf3c6a6dbc4498c4d3
MariaAddict <mashakan27@gmail.com>
2021-01-29 19:11:20 +0700

    feat: создать контроллер createUser и роутер регистрации signup

--- controllers/users.js
+++ controllers/users.js
@@ -1,5 +1,7 @@
+const bcrypt = require('bcryptjs');
 const User = require('../models/user');
 const NotFoundError = require('../errors/not-found');
+const ValidationError = require('../errors/validation-error');
 
 const getUser = (req, res, next) => User.findById(req.user._id)
   .then((data) => {
@@ -16,4 +18,41 @@ const getUser = (req, res, next) => User.findById(req.user._id)
     return next(err);
   });
 
-module.exports = { getUser };
+const createUser = (req, res, next) => {
+  const {
+    email, password, name,
+  } = req.body;
+
+  if (!email || !password) {
+    const err = new Error('Некорректные данные пользователя');
+    err.statusCode = 400;
+    return next(err);
+  }
+
+  return User.findOne({ email }).then((user) => {
+    if (user) {
+      const err = new Error('Пользователь уже зарегистрирован');
+      err.statusCode = 409;
+      return next(err);
+    }
+    return bcrypt.hash(password, 10)
+      .then((hash) => User.create({
+        email, password: hash, name,
+      }))
+      .then((data) => res.send({
+        email: data.email,
+        name: data.name,
+        _id: data._id,
+      }))
+      .catch((err) => {
+        if (err.name === 'ValidationError') {
+          const error = new ValidationError('Некорректные данные пользователя');
+          return next(error);
+        }
+        return next(err);
+      });
+  })
+    .catch(next);
+};
+
+module.exports = { getUser, createUser };
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 const usersRouter = require('./users');
 const articlesRouter = require('./articles');
+const { createUser } = require('../controllers/users');
 
+router.post('/signup', createUser);
 router.use('/', usersRouter);
 router.use('/', articlesRouter);
 
